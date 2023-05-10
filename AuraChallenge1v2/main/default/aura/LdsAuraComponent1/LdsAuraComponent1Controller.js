1481a55341d088688434e917fc86d00d88ac490e
Sam Kuhn <kuhnsamuel2@gmail.com>
2023-05-10 15:19:40 -0500

    added data table - no auto-refresh

--- AuraChallenge1v2/main/default/aura/LdsAuraComponent1/LdsAuraComponent1Controller.js
+++ AuraChallenge1v2/main/default/aura/LdsAuraComponent1/LdsAuraComponent1Controller.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
+    init : function(component, event, helper) {
+        component.set('v.columns', [
+            { label: 'Account Name', fieldName: 'Name', type: 'text'},
+            { label: 'Industry', fieldName: 'Industry', type: 'text'},
+            { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency'}
+        ]);
+        helper.getData(component);
     }
 })
--- AuraChallenge1v2/main/default/aura/LdsAuraComponent1/LdsAuraComponent1Helper.js
+++ AuraChallenge1v2/main/default/aura/LdsAuraComponent1/LdsAuraComponent1Helper.js
@@ -1,5 +1,15 @@
 ({
-    helperMethod : function() {
-
+    getData : function(component) {
+        var action = component.get('c.getAccounts');
+        action.setCallback(this, $A.getCallback(function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set('v.accounts', response.getReturnValue());
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                console.error(errors);
+            }
+        }));
+        $A.enqueueAction(action);
     }
 })
