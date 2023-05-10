119661016935e92107c7a51c36952c8ddc6a90b2
Sam Kuhn <kuhnsamuel2@gmail.com>
2023-05-10 16:19:23 -0500

    Revert "add refresh on account creation"
    This reverts commit f93e4b1a641471b6d357d7e02741729c6784f1bc.

--- AuraChallenge1v2/main/default/aura/LdsAuraComponent1/LdsAuraComponent1Controller.js
+++ AuraChallenge1v2/main/default/aura/LdsAuraComponent1/LdsAuraComponent1Controller.js
@@ -6,9 +6,5 @@
             { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency'}
         ]);
         helper.getData(component);
-    },
-    
-    handleCreateAccount : function(component, event, helper){
-        helper.getData(component);
     }
 })
